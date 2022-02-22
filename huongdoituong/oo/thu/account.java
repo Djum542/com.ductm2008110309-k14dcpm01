5c3fd10cba709a3bdb0a9a8da577a6d64533663c
Djum542 <djum54022@gmail.com>
2022-02-22 13:12:28 +0700

    tai khoan

---
+++ huongdoituong/oo/thu/account.java
@@ -0,0 +1,23 @@
+package huongdoituong.oo.thu;
+
+public class account {
+    String tenTK;
+    String MK;
+    String HoTen;
+    String Tuoi;
+    String Gioitinh;
+    String anhCM;
+
+    public void TenTK() {
+        String tenTK = new String();
+        System.out.println();
+    }
+
+    public void MK() {
+        String MK = new String();
+        System.out.printf(MK);
+    }this.Gioitinh=Gioitinh;this.Tuoi=old;this.HoTen=
+
+    ho va ten;
+
+}
---
+++ huongdoituong/oo/thu/accountTest.java
@@ -0,0 +1,11 @@
+package huongdoituong.oo.thu;
+
+public class accountTest {
+    public static void main(String[] args) {
+        account Account = new account();
+
+        Account.tenTK();
+
+        Account.MK();
+    }
+}
