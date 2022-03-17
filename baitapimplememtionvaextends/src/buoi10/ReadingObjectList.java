fc74ffa13c84a9680080f434b8bbe333dc27346e
Djum542 <djum54022@gmail.com>
2022-03-17 14:22:32 +0700

    tac

---
+++ baitapimplememtionvaextends/src/buoi10/ReadingObjectList.java
@@ -0,0 +1,12 @@
+package src.buoi10;
+
+import java.io.FileInputStream;
+import java.io.IOError;
+import java.io.ObjectInputStream;
+
+public class ReadingObjectList {
+    public static void main(String[] args) throws IOError, ClassNotFoundException {
+        FileInputStream InFile = new FileInputStream(data.dat);
+        ObjectInputStream objIn = new ObjectInputStream(data.dat);
+    }
+}
---
+++ baitapimplememtionvaextends/src/buoi10/WritingObject.java
@@ -0,0 +1,20 @@
+package src.buoi10;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WritingObject {
+    public static void main(String[] args) throws IOException {
+        FileOutputStream outFile = new ObjectOutputStream("data.dat");
+        List<HangThucPham> htp=new ArrayList<>();
+        List.add(htp1);
+        List.add.(htp2);
+        ObjectOutput.WritingObject(lít);
+        System.out.println(Htp);
+        ObjectOutput.close();
+    }
+}
