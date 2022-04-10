9700d69ab33399c3f40e086bb69aea732abc067d
Djum542 <djum54022@gmail.com>
2022-04-10 15:10:24 +0700

    em nộp bài tiểu luận cuối kì cho thay ạ

--- baitapimplememtionvaextends/src/TieuLuanCK/HangHoaClient.java
+++ baitapimplememtionvaextends/src/TieuLuanCK/HangHoaClient.java
@@ -5,52 +5,95 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.sql.Date;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 import javax.swing.JFrame;
-import javax.xml.crypto.Data;
 
 public class HangHoaClient extends JFrame {
     private static final String HangHoa = null;
 
     public static void main(String[] args) throws IOException {
-        FileOutputStream outFile = new FileOutputStream(HangHoa);
-        ObjectOutputStream objOut = new ObjectOutputStream(outFile);
+        try
 
+        {
+            FileOutputStream outFile = new FileOutputStream("data.HangHoa");
+            ObjectOutputStream objOut = new ObjectOutputStream(outFile);
+            System.out.println();
+            objOut.close();
+        } finally {
+            FileOutputStream outFile = new FileOutputStream("data.HangHoa");
+            ObjectOutputStream objOut = new ObjectOutputStream(outFile);
+            objOut.writeObject(HangHoa);
+            System.out.println();
+            objOut.close();
+        }
         Scanner in = new Scanner(System.in);
         DSHangHoa dHoa = new DSHangHoa();
-        System.out.println(HangHoa);
-        objOut.close();
-        while (dHoa != null) {
-            System.out.println("Nhập vào mã hàng hóa:");
-            String maHH = in.nextLine();
-            System.out.println("Nhập tên hàng hóa:");
-            String tenHH = in.nextLine();
-            System.out.println("Nhập số lượng tồn kho:");
-            double soLuongTon = in.nextDouble();
-            System.out.println("Nhập đơn giá của hàng hóa:");
-            double donGia = in.nextDouble();
-            System.out.println("Hàng hóa Thucpham_1/DienMay_2/SanhSu_3 >>");
-            int loai = in.nextInt();
-            HangHoa hangHoa;
-            if (loai == 1) {
 
-                System.out.println("Nhập thông tin của hàng thực phẩm cần thêm");
-                int nsx = in.nextInt();
-                int hsd = in.nextInt();
-                hangHoa = new ThucPham(maHH, tenHH, soLuongTon, donGia, nsx, hsd);
-            } else if (loai == 2) {
-                System.out.println("Nhập thông tin cua hàng điện máy cần thêm");
-                hangHoa = new DienMay(maHH, tenHH, soLuongTon, donGia);
-            } else {
-                System.out.println("Nhập thông tin hàng sanh sứ cần thêm vào");
-                hangHoa = new SanhSu(maHH, tenHH, soLuongTon, donGia);
+        while (dHoa != null) {
+            try {
+                System.out.println("Nhập vào mã hàng hóa:");
+                String maHH = in.nextLine();
+                System.out.println("Nhập tên hàng hóa:");
+                String tenHH = in.nextLine();
+                System.out.println("Nhập số lượng tồn kho:");
+                double soLuongTon = in.nextDouble();
+                System.out.println("Nhập đơn giá của hàng hóa:");
+                double donGia = in.nextDouble();
+                System.out.println("Hàng hóa Thucpham_1/DienMay_2/SanhSu_3 >>");
+                int loai = in.nextInt();
+                HangHoa hangHoa;
+
+                if (loai == 1) {
+
+                    System.out.println("Nhập thông tin của hàng thực phẩm cần thêm");
+                    int nsx = in.nextInt();
+                    int hsd = in.nextInt();
+                    hangHoa = new ThucPham(maHH, tenHH, soLuongTon, donGia, nsx, hsd);
+                } else if (loai == 2) {
+                    System.out.println("Nhập thông tin cua hàng điện máy cần thêm");
+                    hangHoa = new DienMay(maHH, tenHH, soLuongTon, donGia);
+                } else {
+                    System.out.println("Nhập thông tin hàng sanh sứ cần thêm vào");
+                    hangHoa = new SanhSu(maHH, tenHH, soLuongTon, donGia);
+
+                }
+            } catch (Exception e) {
+                System.out.println("Nhap ky tu ban ơi");
+                e.printStackTrace();
+            } finally {
+                System.out.println("Nhập vào mã hàng hóa:");
+                String maHH = in.nextLine();
+                System.out.println("Nhập tên hàng hóa:");
+                String tenHH = in.nextLine();
+                System.out.println("Nhập số lượng tồn kho:");
+                double soLuongTon = in.nextDouble();
+                System.out.println("Nhập đơn giá của hàng hóa:");
+                double donGia = in.nextDouble();
+                System.out.println("Hàng hóa Thucpham_1/DienMay_2/SanhSu_3 >>");
+                int loai = in.nextInt();
+                HangHoa hangHoa;
+
+                if (loai == 1) {
+
+                    System.out.println("Nhập thông tin của hàng thực phẩm cần thêm");
+                    int nsx = in.nextInt();
+                    int hsd = in.nextInt();
+                    hangHoa = new ThucPham(maHH, tenHH, soLuongTon, donGia, nsx, hsd);
+                } else if (loai == 2) {
+                    System.out.println("Nhập thông tin cua hàng điện máy cần thêm");
+                    hangHoa = new DienMay(maHH, tenHH, soLuongTon, donGia);
+                } else {
+                    System.out.println("Nhập thông tin hàng sanh sứ cần thêm vào");
+                    hangHoa = new SanhSu(maHH, tenHH, soLuongTon, donGia);
+
+                }
 
             }
+            return;
         }
+        System.out.println("Ban co nhap tiep không y/n");
+        int nhap = in.nextInt();
         int x = in.nextInt();
 
         do {
@@ -100,5 +143,4 @@ public class HangHoaClient extends JFrame {
         super();
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
-
 }
